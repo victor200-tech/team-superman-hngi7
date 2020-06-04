28ed65bcf72ba9a03763e71c1801ac5b4452ce69
victor200-tech <55100684+victor200-tech@users.noreply.github.com>
2020-06-04 17:44:10 +0100

    Update segun33.js

--- scripts/segun33.js
+++ scripts/segun33.js
@@ -1,6 +1,12 @@
 
-const Name = OGUNEKUN SEGUN VICOR;
-const HNGID = HNG-03092;
-const email = ogunekun@gmail.com;
-const text = Hello world This is $ name 
-}
+const Name = "OGUNEKUN SEGUN VICOR";
+
+const id = "HNG-03092";
+
+const email = "ogunekun@gmail.com";
+
+const language = "javascript";
+
+const text = () => {
+    return "Hello world. This is ${name} with HNGI7 ID ${id} using ${language} for stage 2 task.${email}"};
+console.log(text());
